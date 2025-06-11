2730f24627d80e8370f984085729037f44584ba4
Vivek Sharma <mani1980rani@gmail.com>
2025-06-11 23:20:47 +0530

    Add music library for song playback

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music={
+    "warriyo":"https://www.youtube.com/watch?v=yJg-Y5byMMw",
+    "unknown brain":"https://www.youtube.com/watch?v=LHvYrn3FAgI",
+    "skyfall":"https://www.youtube.com/watch?v=sZrTJesvJeo",
+    "royalty":"https://www.youtube.com/watch?v=6LIQtqaetbE",
+    "bones":"https://www.youtube.com/watch?v=ZNsPYmkSPeI"
+}
